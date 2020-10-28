afea9bcd8f019bd8e34f0442be67f58b95878d9c
devops1262 <69254167+devops1262@users.noreply.github.com>
2020-10-28 09:45:48 +0530

    Update amldeploy.sh

--- amldeploy.sh
+++ amldeploy.sh
@@ -3,6 +3,7 @@ echo "Deployment Started"
 ssh jenkins@52.175.250.60 'sh /home/jenkins/apache-tomcat-8.5.59/bin/shutdown.sh'
 ssh jenkins@52.175.250.60 'mkdir -p /home/jenkins/apache-tomcat-8.5.59/webapps/backup'
 ssh jenkins@52.175.250.60 'cp /home/jenkins/apache-tomcat-8.5.59/webapps/hms.war /home/jenkins/apache-tomcat-8.5.59/webapps/backup'
+mv target/hms*.war target/hms.war
 scp target/hms.war jenkins@52.175.250.60:/home/jenkins/apache-tomcat-8.5.59/webapps
 ssh jenkins@52.175.250.60 'sh /home/jenkins/apache-tomcat-8.5.59/bin/startup.sh'
 echo "Deployment has been completed successfully"
