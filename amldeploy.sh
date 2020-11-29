3137333e4ea43029542692c3db1c659a27efa6a2
devops1262 <69254167+devops1262@users.noreply.github.com>
2020-11-29 11:07:17 +0530

    Update amldeploy.sh

--- amldeploy.sh
+++ amldeploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Deployment Started"
-ssh jenkins@52.175.250.60 'sh /home/jenkins/apache-tomcat-8.5.59/bin/shutdown.sh'
-ssh jenkins@52.175.250.60 'mkdir -p /home/jenkins/apache-tomcat-8.5.59/webapps/backup'
-ssh jenkins@52.175.250.60 'cp /home/jenkins/apache-tomcat-8.5.59/webapps/hms.war /home/jenkins/apache-tomcat-8.5.59/webapps/backup'
+ssh jenkins@168.61.50.213 'sh /home/jenkins/apache-tomcat-8.5.60/bin/shutdown.sh'
+ssh jenkins@168.61.50.213 'mkdir -p /home/jenkins/apache-tomcat-8.5.60/webapps/backup'
+ssh jenkins@168.61.50.213 'cp /home/jenkins/apache-tomcat-8.5.60/webapps/hms.war /home/jenkins/apache-tomcat-8.5.60/webapps/backup'
 mv target/hms*.war target/hms.war
-scp target/hms.war jenkins@52.175.250.60:/home/jenkins/apache-tomcat-8.5.59/webapps
-ssh jenkins@52.175.250.60 'sh /home/jenkins/apache-tomcat-8.5.59/bin/startup.sh'
+scp target/hms.war jenkins@168.61.50.213:/home/jenkins/apache-tomcat-8.5.60/webapps
+ssh jenkins@168.61.50.213 'sh /home/jenkins/apache-tomcat-8.5.60/bin/startup.sh'
 echo "Deployment has been completed successfully"
